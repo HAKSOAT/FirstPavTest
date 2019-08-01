b6abe9bb15abbbaffdf09c418a127cfde632d038
haksoat <shopejuh@gmail.com>
2019-08-01 21:18:30 +0100

    Refactored file

--- run_server.py
+++ run_server.py
@@ -2,9 +2,9 @@
 from app.app import create_app, register_extensions
 from app.config.config import DevelopmentConfig
 
-# Create an app instance and run
+app = create_app(DevelopmentConfig)
+register_extensions(app)
+
 if __name__ == '__main__':
-    app = create_app(DevelopmentConfig)
-    register_extensions(app)
     PORT = int(app.config['PORT'])
     app.run(port=PORT)
